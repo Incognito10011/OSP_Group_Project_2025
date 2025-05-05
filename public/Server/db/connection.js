6dd43ac1811e178b0bb8eef4bb4420ff39389b3c
JJLane <156961444+JJLane13@users.noreply.github.com>
2025-05-05 21:17:06 +0100

    Add files via upload

---
+++ public/Server/db/connection.js
@@ -0,0 +1,50 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+// supposed to load the .env file into process.env
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.ATLAS_URI;
+
+if (!uri) {
+  throw new Error("Missing ATLAS_URI in .env");
+}
+
+const client = new MongoClient(uri, {
+  tls: true,
+  tlsAllowInvalidCertificates: true, // QUICK MAC FIX, should be able to connect reguardless of tls handshake
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// UNCOMMENT FOR FULL RELEASE, THIS IS A QUICK FIX BUT NOT SECURE
+
+//const client = new MongoClient(uri, {
+//    serverApi: {
+//        version: ServerApiVersion.v1,
+//        strict: true,
+//        deprecationErrors: true,
+//    },
+//});
+
+
+//async function added, allows the await command to work, should.
+async function connectToDatabase() {
+try {
+    // connect client to the server
+    await client.connect();
+    //send a ping to confirm successful connection
+    await client.db("admin").command({ ping: 1 });-
+    console.log(
+    "pinged your deployment. you successfully conted to mongoDB!"
+    );
+} catch(err) {
+  console.error(err);
+}
+}
+// in mongo db, this is the name of the collection, under the tab "collections"
+let db = client.db("BlogProj");
+
+export default db;
